6c732d0e738d2132fed144e58cd186d67324c000
Carlos <carlos@falconmasters.com>
2021-10-22 15:55:44 -0500

    Agregando readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Animaciones al Scrollear con Intersection Observer | Javascript
+### [Tutorial: https://youtu.be/cVsqA4NhDoI](https://youtu.be/cVsqA4NhDoI)
+
+![Animaciones al Scrollear con Intersection Observer | Javascript](https://raw.githubusercontent.com/falconmasters/intersection-observer/master/img/thumb.png)
+
+Por: [FalconMasters](http://www.falconmasters.com)
---
+++ js/app.js
@@ -0,0 +1,24 @@
+const imagen1 = document.getElementById('imagen1');
+const imagen2 = document.getElementById('imagen2');
+
+const cargarImagen = (entradas, observador) => {
+	// console.log(entradas)
+	// console.log(observador)
+
+	entradas.forEach((entrada) => {
+		if(entrada.isIntersecting){
+			entrada.target.classList.add('visible');
+		} else {
+			// entrada.target.classList.remove('visible');
+		}
+	});
+}
+
+const observador = new IntersectionObserver(cargarImagen, {
+	root: null,
+	rootMargin: '500px 0px 100px 0px',
+	threshold: 1.0
+});
+
+observador.observe(imagen1);
+observador.observe(imagen2);
